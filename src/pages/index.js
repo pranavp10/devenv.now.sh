1d27dffc97e84b3bf2e14cb68de61cf8db4fa710
Pranav Patel <pranav@quze.co>
2020-07-05 13:46:52 +0530

    initial setup

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import {
+  Link as ChakraLink,
+  Text,
+  Code,
+  Icon,
+  List,
+  ListIcon,
+  ListItem,
+} from '@chakra-ui/core'
+
+
+const Index = () => (
+  <h1>Hello</h1>
+)
+
+export default Index
